f1219bbe5b48ff487243d07db6c74f6b9e624db0
bowen.deng <bowen.deng@dmall.com>
2018-11-09 16:32:13 +0800

    修改部分文件

--- tms/tms-web/src/main/java/com/ergroup/tmsweb/controller/OrderController.java
+++ tms/tms-web/src/main/java/com/ergroup/tmsweb/controller/OrderController.java
@@ -2,9 +2,23 @@ package com.ergroup.tmsweb.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/order")
 public class OrderController {
+
+    @GetMapping("/createorder")
+    public String createOrder() {
+
+        return "order/createorder";
+    }
+
+    @GetMapping("/hisorder")
+    public String hisOrder() {
+
+        return "order/hisorder";
+    }
 
     @GetMapping("/order")
     public String order() {
